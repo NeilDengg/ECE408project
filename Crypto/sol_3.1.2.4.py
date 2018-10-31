14de90a2a93cfefbda56e07087b1878df84dc580
ubuntu <ubuntu@ubuntu.(none)>
2018-10-31 14:43:25 -0400

    CheckPoint 1

--- Crypto/sol_3.1.2.4.py
+++ Crypto/sol_3.1.2.4.py
@@ -9,8 +9,7 @@ def decipher(input,keys,modulo,outfile):
 	with open(modulo) as modulo:
 		modulo=modulo.read().strip()
 	ans=pow(int(string,16),int(keys,16),int(modulo,16))
-	ans=str(hex(ans))[2:]
+	ans=str(hex(ans))[2:-1]
 	with open(outfile,"w") as outfile:
 		outfile.write(ans)
-		print ans
 decipher(sys.argv[1],sys.argv[2],sys.argv[3],sys.argv[4])
